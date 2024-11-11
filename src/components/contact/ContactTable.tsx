a25b9d8692b92784ed1ab471ce1a528a312ad65e
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2024-11-11 12:19:30 -0300

    deselect filters

--- src/components/contact/ContactTable.tsx
+++ src/components/contact/ContactTable.tsx
@@ -66,13 +66,11 @@ export default function ContactTable(_props: Props): JSX.Element {
           <Select
             placeholder="Seleccione una categoria"
             data={contactCategories}
-            allowDeselect={false}
             onChange={(value) => handleFilterChange("category", value || "")}
           />
           <Select
             placeholder="Seleccione un estado"
             data={contactStatus}
-            allowDeselect={false}
             onChange={(value) => handleFilterChange("status", value || "")}
           />
         </Group>
